3da3a25d022d97e4a150bcafd90b7532950a0a4c
hossainzarif <57715518+hossainzarif@users.noreply.github.com>
2020-03-26 17:02:47 +0600

    Done with initial Design

---
+++ TutorAssistant/app/src/main/java/com/example/tutorassistant/Add_Daily_Reminder.java
@@ -0,0 +1,14 @@
+package com.example.tutorassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Daily_Reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__daily__reminder);
+    }
+}
--- TutorAssistant/app/src/main/java/com/example/tutorassistant/Add_reminder.java
+++ TutorAssistant/app/src/main/java/com/example/tutorassistant/Add_reminder.java
@@ -2,13 +2,37 @@ package com.example.tutorassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Add_reminder extends AppCompatActivity {
 
+    Button button_confirm ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+
+
+        button_confirm =(Button)findViewById(R.id.button_for_confirm) ;
+
+        button_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity() ;
+            }
+        });
+
     }
+
+    public void openActivity()
+    {
+        Intent intent = new Intent(this,Add_Daily_Reminder.class) ;
+        startActivity(intent);
+    }
+
+
+
 }
